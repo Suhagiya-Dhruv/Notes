ab7c3b9884bd39ceb111965785d3db5aad12a905
Dhruv Suhagiya <dhruv.suhagiya11@gmail.com>
2023-10-06 16:54:54 +0530

    Note updated

---
+++ pratic.java
@@ -0,0 +1,59 @@
+
+class Operators {
+
+    public static void main(String[] arg) {
+
+        /**
+         * sum + minus - multiply * division / module %
+         *
+         */
+        // int a = 10;
+        // int b = 20;
+        
+        // int ans = b%a;
+        // System.out.println("Ans "+ans);
+        // int a = 10;
+        // a++; // a = a+1; // post increment // 11
+        
+        // ++a; // a = 1+a; // pre increment // 12
+    
+        // System.out.println("Ans "+a);
+
+
+        // int a = 10;
+        // a--; // a = a-1; // post increment // 9
+        //
+        // --a; // a = 1-a; // pre increment // 8
+        //
+        // System.out.println("Ans "+a);
+
+        // int a = 10;
+        // System.out.println(a--); // a = 1+a -> print
+        // System.out.println(a);
+
+        // boolean = true/false
+        // ==, >, <, >= , <=, != return true/false
+
+        // int a = 10;
+        // System.out.println(a != 11); // 10 != 10
+
+        
+        // Logical &&(AND), ||(OR), !(NOT)
+        
+        // =, +=, -=, *=, /=, %=
+
+        // int a = 10;
+        // a -= 5; // a = a+5; => Arithmetic -> assign
+        // System.out.println(a);
+
+        // if (a == 10) {
+        // System.out.println("YES "+a);
+        // } else {
+        // System.out.println("NO");
+        // }
+
+        // int a = 10;
+        // System.out.println(a == 10 ? "YES" : "NO");
+
+    }
+}
