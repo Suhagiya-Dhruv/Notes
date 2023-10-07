98a52ade3ec8192b3a1d5ab6ddcbc1cd556bf047
Dhruv Suhagiya <dhruv.suhagiya11@gmail.com>
2023-10-07 15:07:00 +0530

    Loop Notes

--- main.java
+++ main.java
@@ -1,59 +1,155 @@
+/* loop
 
-class Operators {
+switch
 
-    public static void main(String[] arg) {
+for loop
+while
+do while
 
-        /**
-         * sum + minus - multiply * division / module %
-         *
+
+array
+stack => LIFO => last in first out
+queue => FIFO => first in first out
+
+ */
+
+class Loop {
+
+    public static void main(String[] args) {
+
+//        int a = 10;
+//        int b = 15;
+//        char c = '+';
+//
+//        switch(c){
+//            case '+': System.out.println(a+b);
+//                    break;
+//            case '-': System.out.println(a-b);
+//                    break;
+//            case '*': System.out.println(a*b);
+//                 break;
+//            case '/': System.out.println(a/b);
+//                    break;
+//            default : System.out.println("Invalid value");
+//                    break;
+//        }
+//        if (c == '*') {
+//            System.out.println(a * b);
+//        } else if (c == '+') {
+//            System.out.println(a + b);
+//        } else if (c == '-') {
+//            System.out.println(a - b);
+//        } else if (c == '/') {
+//            System.out.println(a / b);
+//        }
+//        System.out.println("Outside");
+        // value assign ; condition ; inc/dec
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println("I :"+i);
+//        }
+//        for (int i = 5; i >= 0;i--) { // 5>0; 4>0
+//            System.out.println(i);
+//            
+////            i = i+2;
+//        }
+// condition
+//        int a = 1;
+//        while (a < 10) {  // 15 < 10
+//            System.out.println(a);  // 1 , 3 , 7
+//            a *= 2; // a = 1*2 = 2 =6 = 14
+//            System.out.println(a); // 2 , 6 , 14
+//            a++; // 3, 7, 15
+//        }
+        // 1, 2, 3 ,6, 7,14
+//      int a = 1;
+//      do{
+//          System.out.println(a);
+//          a++; 
+//      }while(a < 10); // 2<5, 3<5, 4<5, 5<5
+        // 1,2,3,6,7
+        // 
+        //
+        /*
+        
+        i = 0 -> i < 5 => T
+                Hello
+        i++; -> i = i+1
+        
+        
+        i = 1 -> i< 5 => T
+                Hello
+        i++;
+        
+        i=2 -> i < 5 => T
+            hello
+        
+        .
+        .
+        .
+        .
+        .
+        
+        i = 4 -> i<5 ; T
+        i++;
+        
+        i = 5 -> 5<5 ; F
+        
+        
          */
-        // int a = 10;
-        // int b = 20;
+//        int a[] = {1,3,5,2,4,6};
+        // index -> [0,1,2,3,4,5]
+        // value -> [1,3,5,2,4,6]
+//        System.out.println(a[0]+a[1]+a[2]+a[3]+a[4]+a[5]);
+//        int sum = 0; // 4
+//       
+//        for (int i = 0; i <= 5; i++) { // 1
+//            sum = sum + a[i];
+//            System.out.print(sum + " ");
+//        }
+//        System.out.println();
+//        System.out.println(sum);
+//        int a[] = {1, 3, 5, 2, 4, 6};
+//        int oddSum = 0;
+//        int evenSum = 0;
+//
+//        for (int i = 0; i <= 5; i++) {
+//            if (a[i] % 2 == 0) {
+//                evenSum = evenSum + a[i];
+//            } else {
+//                oddSum = oddSum + a[i];
+//            }
+//        }
+//
+//        System.out.println(oddSum);
+//        System.out.println(evenSum);
+//        int a[] = {1, 3, 5, 2, 4, 6};
+//
+//        int evenIndex = 0;
+//
+//        for (int i = 0; i <= 5; i += 2) {
+//            evenIndex = evenIndex + a[i];
+//        }
+//
+//        System.out.println(evenIndex);
+
+
+
+//          2^2 = 4 => 2^8 = 2*2*2*2 = 16 =>  2^8 = 256
+
+//        int ans = 1;
+//        int sq = 8;
         
-        // int ans = b%a;
-        // System.out.println("Ans "+ans);
-        // int a = 10;
-        // a++; // a = a+1; // post increment // 11
-        
-        // ++a; // a = 1+a; // pre increment // 12
-    
-        // System.out.println("Ans "+a);
+//        for(int i=0;i<sq;i++){
+//            ans = ans*2;
+//        }
 
-
-        // int a = 10;
-        // a--; // a = a-1; // post increment // 9
-        //
-        // --a; // a = 1-a; // pre increment // 8
-        //
-        // System.out.println("Ans "+a);
-
-        // int a = 10;
-        // System.out.println(a--); // a = 1+a -> print
-        // System.out.println(a);
-
-        // boolean = true/false
-        // ==, >, <, >= , <=, != return true/false
-
-        // int a = 10;
-        // System.out.println(a != 11); // 10 != 10
-
-        
-        // Logical &&(AND), ||(OR), !(NOT)
-        
-        // =, +=, -=, *=, /=, %=
-
-        // int a = 10;
-        // a -= 5; // a = a+5; => Arithmetic -> assign
-        // System.out.println(a);
-
-        // if (a == 10) {
-        // System.out.println("YES "+a);
-        // } else {
-        // System.out.println("NO");
-        // }
-
-        // int a = 10;
-        // System.out.println(a == 10 ? "YES" : "NO");
-
+//        while(sq>0){
+//            ans *= 2;
+//            sq--;
+//        }
+//
+//        System.out.println(" 2^"+sq+" = "+ans); // 256
     }
 }
+
+// Question =  Check in array even number is exits or not int a = [1,3,5,7,9,11] => False, a = [1,2,5,6,7,9]=> True, 2
--- readme.md
+++ readme.md
@@ -1,53 +1,76 @@
-## Type of Operators
+### Switch Statement in Java:
 
----
+A switch statement allows you to select one of many code blocks to be executed based on the value of an expression. It's often used when you have multiple possible cases to handle.
 
-1. Arithmetic Operators
-   - These operators are used to perform arithmetic/mathematical operations on operands. Examples: (+, -, \*, /, %)
-2. Increment Operators
-   - This operatora are used to perform increment by one (++)
-   - post increment
-   - pre increment
-3. Decrement Operators
-   - This operatora are used to perform decrement by one (--)
-   - post decrement
-   - pre decrement
-4. Relational Operators
-   - These are used for the comparison of the values of two operands. For example, checking if one operand is equal to the other operand or not, whether an operand is greater than the other operand or not, etc. Some of the relational operators are (==, >, <, >= , <=, != )
-5. Logical Operators - Logical Operators are used to combining two or more conditions/constraints or to complement the evaluation of the original condition in consideration. The result of the operation of a logical operator is a Boolean value either true or false.
+Example:
 
-   - AND (&&)
-     |Value 1| Value 2 | Answer |
-     |--|--|--|
-     | 0 |0 |0 |
-     | 0 |1 |0 |
-     | 1 |0 |0 |
-     | 1 |1 |1 |
-   - OR (||)
-     |Value 1| Value 2 | Answer |
-     |--|--|--|
-     | 0 |0 |0 |
-     | 0 |1 |1 |
-     | 1 |0 |1 |
-     | 1 |1 |1 |
-   - Not (!)
-     |Value 1| Answer |
-     |--|--|
-     | 0 | 1 |
-     | 1 |0 |
+```java
+int day = 3;
+String dayName;
 
-6. Bitwise Operators (Not requirement)
-   - The Bitwise operators are used to perform bit-level operations on the operands. The operators are first converted to bit-level and then the calculation is performed on the operands. Mathematical operations such as addition, subtraction, multiplication, etc. can be performed at the bit level for faster processing.
-   - AND (&)
-   - OR (|)
-   - Right shift (>>)
-   - Left shift (<<)
-   - Not(~)
-   - XOR(^)
-7. Assignment Operators
-   - Assignment operators are used to assign value to a variable. The left side operand of the assignment operator is a variable and the right side operand of the assignment operator is a value. The value on the right side must be of the same data type as the variable on the left side otherwise the compiler will raise an error.
-   - Example (=, +=, -=, \*=, /=, %=)
-8. Conditional Operator
-   - The conditional operator is of the form ( ? : )
-   - Example (Expression1? Expression2: Expression3)
-   - Here, Expression1 is the condition to be evaluated. If the condition(Expression1) is True then we will execute and return the result of Expression2 otherwise if the condition(Expression1) is false then we will execute and return the result of Expression3.
+switch (day) {
+    case 1:
+        dayName = "Monday";
+        break;
+    case 2:
+        dayName = "Tuesday";
+        break;
+    case 3:
+        dayName = "Wednesday";
+        break;
+    // More cases can be added as needed
+    default:
+        dayName = "Invalid day";
+}
+
+System.out.println("Today is " + dayName);
+
+```
+
+In this example, the switch statement evaluates the value of day, and based on its value (in this case, 3), it assigns the corresponding dayName and prints it.
+
+### For Loop in Java:
+
+A for loop is used to execute a block of code a specified number of times, making it useful for tasks that require iteration.
+
+Example:
+
+```java
+for (int i = 1; i <= 5; i++) {
+    System.out.println("Iteration " + i);
+}
+```
+
+This loop will print "Iteration 1" through "Iteration 5" because it starts with i equal to 1, increments it by 1 in each iteration, and stops when i becomes greater than 5.
+
+### While Loop in Java:
+
+A while loop repeatedly executes a block of code as long as a specified condition is true.
+
+Example:
+
+```java
+int count = 1;
+while (count <= 5) {
+    System.out.println("Count is: " + count);
+    count++;
+}
+```
+
+This while loop prints "Count is: 1" through "Count is: 5" because it continues as long as count is less than or equal to 5.
+
+### Do-While Loop in Java:
+
+A do-while loop is similar to a while loop, but it guarantees that the code block is executed at least once before checking the loop condition.
+
+Example:
+
+```java
+int num = 1;
+do {
+    System.out.println("Number is: " + num);
+    num++;
+} while (num <= 5);
+```
+
+This do-while loop prints "Number is: 1" through "Number is: 5" because it executes the code block once, and then it checks the condition.
