641800d55496b146dffcf44019bcad1257008ec5
Dhruv Suhagiya <dhruv.suhagiya11@gmail.com>
2023-10-11 19:30:28 +0530

    Function, var, let and const

---
+++ exec.js
@@ -0,0 +1,183 @@
+// var a = {
+//     name: "Dhruv",
+//     address: {
+//         city:"surat",
+//         pincode: 365626,
+//         deatils:{
+
+//         }
+//     }
+// }
+
+// a.name = "...."
+// a.address.city="hello"
+// a.address.deatils.sayHi="HI"
+
+// console.log(a)
+
+// var b = [1,2,3,4,5,6];
+// var c = b;
+
+// b.pop();
+
+// console.log(b);
+// console.log(c);
+
+
+// var d = {
+//     name: "Dhruv",
+//     address: {
+//         city: "surat",
+//         pincode: 365626,
+//         deatils: {
+
+//         }
+//     }
+// }
+
+// var e = d;
+
+// console.log(e.name)
+
+// delete e.address
+
+// console.log(d)
+// console.log(e)
+
+// var b = 10;
+// var c = b;
+
+// b = 15;
+
+// console.log(b)
+// console.log(c)
+
+// var b = [1, 2, 3, 4, 5, 6]; // travel
+// console.log(b)
+
+// for (var i = 0; i < b.length; i++) {
+//     console.log(i, "=> ", b[i]);
+// }
+
+// for (var i of b) {
+//     console.log(i);
+// }
+
+// for (var i in b) {
+//     console.log(i);
+// }
+// console.log(b)
+
+// var d = {
+//     name: "Dhruv",
+//     address: "surat",
+//     pincode: 265656
+// }
+
+// var key1 ="name";
+// var key2 ="address"
+// var key3 = "pincode"
+
+// console.log(d[key1])
+// console.log(d[key2])
+// console.log(d[key3])
+
+// for in
+
+// for(var a in d){
+//     console.log(a, "=>", d[a])
+// }
+
+// console.log(d)
+
+// function js(){   // create a function
+//     console.log("hello");
+// }
+
+// js(); // call the function ()=>call
+
+// console.log(js) // print function
+// console.log(js())
+
+// function js(){   // create a function
+//     console.log("hello");
+//     return "BYE";
+// }
+
+// js(); // call the function ()=>call
+
+// console.log(js) // print function
+// console.log(js())
+
+// function js(){   
+//     return "hello";
+// }
+
+// var ans = js(); 
+
+// console.log(ans)
+
+
+// parameter argument
+// I need to create a funcation 2*2 =4 , 5*5 = 25
+
+// function js(a){    // argument 
+//     return a*a
+// }
+
+// var ans = js(1);  // parameter pass
+
+// console.log(ans)
+
+// function sum(a,b){ 
+//     return a+b;
+// }
+
+// var ans = sum(1,2);
+
+// console.log(ans)
+
+
+//declaration
+// var a;
+// let b;
+// const c;
+
+// initialzation
+// var a = 10;
+// let b = 10;
+// const c = 10;
+
+
+// re intialization
+// var a = 10;
+// a = 20;
+
+// console.log(a)
+
+// let b = 10;
+// b = 20;
+
+// console.log(b)
+
+// const c = 10;
+// c = 20;
+
+// console.log(c)
+
+// re declaration 
+
+// var a = 10;
+// var a = 15;
+
+// console.log(a)
+
+// let b = 10;
+// let b = 15;
+
+// console.log(b)
+
+// const c = 10;
+// const c = 15;
+
+// console.log(c)
--- readme.md
+++ readme.md
@@ -3,3 +3,22 @@
 1. Nested Object
 2. Travels Object
 3. Function
+4. var
+   - declaration (**✓**)
+   - assignment/initialzation (**✓**)
+   - re intialization (**✓**)
+   - re declaration (**✓**)
+5. let (ES6)
+   - declaration (**✓**)
+   - assignment/initialzation (**✓**)
+   - re intialization (**✓**)
+   - re declaration (**✕**)
+6. const (ES6)
+   - declaration (**✕**)
+   - assignment/initialzation (**✓**)
+   - re intialization (**✕**)
+   - re declaration (**✕**)
+
+Why Javascirpt => https://www.ample.co/blog/javascript-history
+
+
