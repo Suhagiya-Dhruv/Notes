fb2ea257e051ccd6f5ac813048f2b5d1181ac6bd
Dhruv Suhagiya <dhruv.suhagiya11@gmail.com>
2023-10-12 18:22:04 +0530

    Hoisting & How JS work

---
+++ hoisting.js
@@ -0,0 +1,85 @@
+// Normal Function
+// function run(b) { // argument
+//     console.log("run",b);
+// }
+
+// run(15) // parameter
+
+// Function expression
+// var a = function hello(b) {
+//     console.log("Hello", b)
+// }
+
+// a(10);
+
+// var b = 10;
+
+// console.log(b)
+
+// function hi(a=10){ // a default value 10 -> 15
+//     console.log(a) //
+// }
+
+// hi(15)
+
+// 2, 3 => 2*2*2 = 8
+// 5,3 => 5*5*5 = 125,
+// 2, 10 => 2*2*2*2*2*2*2*2*2*2 => 1024
+
+// function run(a, b) {
+//     var ans = 1;
+//     for (var i = 1; i <= b; i++) {
+//         ans = ans * a;
+//     }
+//     return ans;
+// }
+// var ans = run(5,3);
+
+// console.log(ans)
+
+// var a = 10;
+
+// var b = 15;
+
+
+// function run(){
+//     var a = 20;
+//     console.log("run");
+//     function hello(){
+//         console.log("hello")
+//     }
+//     hello();
+// }
+
+// run();
+// var b;
+// console.log(a);
+// var a = 10; // phase1 : Memory creation phase
+// console.log(a);
+
+
+// var a = 10;
+
+// console.log(a());
+
+
+//  ================  Memory creation ==============
+
+// 
+
+// f1 = undefined
+
+// // ----------- Code Execution -----------
+
+
+// //  f1(); // error => undefined()
+//  console.log(f1)
+
+//  var f1 = function f2(){
+//      console.log("Hello 2")
+//  }
+
+//  f1();
+
+// let a = 10;
+// console.log(a);
