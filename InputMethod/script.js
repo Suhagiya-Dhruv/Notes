02ee4f59267eefd6acc3b275e67c9e09db1f77c6
Dhruv Suhagiya <dhruv.suhagiya11@gmail.com>
2023-12-07 15:15:07 +0530

    Input mathos

---
+++ InputMethod/script.js
@@ -0,0 +1,86 @@
+// const input = document.getElementById("input");
+// const input2 = document.getElementById("range");
+
+// input.addEventListener("input",inputHandler)
+
+// function inputHandler(event){
+//     console.log(event.target.value);
+// }
+
+// input2.addEventListener("input",inputHandler2)
+
+// function inputHandler2(event){
+//     console.log(event.target.value);
+// }
+
+// input.addEventListener("change",inputHandler)
+
+// function inputHandler(event){
+//     console.log(event.target.value);
+// }
+
+// document.addEventListener("keydown", function(event){
+//     console.log("Key Down",event.key)
+// })
+
+// document.addEventListener("keyup", function(event){
+//     console.log("Key Up",event.key)
+// })
+
+// document.addEventListener("keypress", function(event){
+//     console.log("Key Press",event.key)
+// })
+
+
+// document.addEventListener("mousedown", function(event){
+//     console.log("Mouse Down",event)
+// })
+
+// document.addEventListener("mouseup", function(event){
+//     console.log("Mouse Up",event.which)
+// })
+
+
+/*
+
+click
+dblclick
+mousemove
+mouseleave
+mouseenter
+mouseover
+input
+change
+keyup
+keydown
+keypress
+mouseup
+mosseDown
+
+
+resize
+scroll
+submit
+drag
+drop
+
+
+*/
+
+// event capture
+
+
+// debouncing
+
+const data = document.getElementById("btn");
+
+data.addEventListener("click", function () {
+    console.log("account created. user1")
+});
+
+// java -> andriod studio
+
+// web development
+
+// -> javascript -> React
+// -> jQuery
