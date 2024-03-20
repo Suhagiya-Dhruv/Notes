2294ad48fd55f9246a2dd16be08735d0c388919f
Dhruv suhagiya <dhruv.suhagiya11@gmail.com>
2024-03-20 13:08:42 +0530

    APIs

--- index.js
+++ index.js
@@ -1,8 +1,56 @@
 const express = require('express');
+const fs = require('fs');
+const server = express();
 
-const app = express();
+/*
 
-const port = 5000
-app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
+CURD Opration
+
+C - Create 
+U - Update
+R - Read
+D - Delete
+
+API Methods
+
+1. GET - Read
+2. POST - Create
+3. PUT/PATCH - Update
+4. DELETE - Delete
+
+*/
+
+// API HIT -> API Call
+
+
+// http://localhost:5555  -> Domain -> http://chrome.com/  -> API Link
+
+// http://localhost:5555/products  -> Domain -> http://chrome.com/  -> API Link -> END point /products
+// http://localhost:5555/users  -> Domain -> http://chrome.com/  -> API Link -> End Point /users
+// http://localhost:5555/carts  -> Domain -> http://chrome.com/  -> API Link -> End Point /carts
+
+server.get('/products', (req, res) => {
+
+    const data = fs.readFileSync('product.json');
+    res.send(JSON.parse(data))
+
+})
+
+server.get('/products/:id', (req, res) => {
+
+    const id = Number(req.params.id);
+
+    const data = JSON.parse(fs.readFileSync('product.json'));
+
+    const response = data.filter(item => item.id === id);
+
+    if(response.length){
+        res.send(response)
+    }else{
+        res.send("Data not found")
+    }
+})
+
+server.listen(5555, () => {
+    console.log('Server is running on port http://localhost:5555');
 })
